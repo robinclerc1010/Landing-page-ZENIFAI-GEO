765cf75c3ce6a7b9fde3300f4dcc155649028f2d
robinclerc1010 <rclerc26@gmail.com>
2026-04-22 09:58:45 +0200

    Update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
   images: {
     formats: ["image/avif", "image/webp"],
     unoptimized: true,
